479ea2958e8cd7b7fd832765c0e4705b18dd5f9e
Vipul Kithani <2015vipul.kithani@ves.ac.in>
2019-07-14 19:26:18 +0530

    Updates

---
+++ precache-manifest.7e4f80a90fd12b5e55c2fc2387e3ca14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73817f2a2fc2b7192cdad9a9bcb3f01f",
+    "url": "/VipulKithani/mysite/index.html"
+  },
+  {
+    "revision": "6a1bcdd1d5e354d2a658",
+    "url": "/VipulKithani/mysite/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "03f140a13c33e8baefc6",
+    "url": "/VipulKithani/mysite/static/css/main.bc938eb7.chunk.css"
+  },
+  {
+    "revision": "6a1bcdd1d5e354d2a658",
+    "url": "/VipulKithani/mysite/static/js/2.d2f84d4c.chunk.js"
+  },
+  {
+    "revision": "03f140a13c33e8baefc6",
+    "url": "/VipulKithani/mysite/static/js/main.defab068.chunk.js"
+  },
+  {
+    "revision": "b4f3115959cdf22c50e5",
+    "url": "/VipulKithani/mysite/static/js/runtime~main.2087be1e.js"
+  },
+  {
+    "revision": "bd9ede4a870c8beaca8c408499c261af",
+    "url": "/VipulKithani/mysite/static/media/logo.bd9ede4a.png"
+  }
+]);
